9d32642c3e2a22b963e6531763d64365cb257516
Shawky Elsayed <shawkyelsayed2002@gmail.com>
2025-03-11 16:35:38 +0200

    Initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { FeaturedProductsComponent } from './components/featured-products/featured-products.component';
+import { AboutSectionComponent } from './components/about-section/about-section.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroSectionComponent,
+    FeaturedProductsComponent,
+    AboutSectionComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
